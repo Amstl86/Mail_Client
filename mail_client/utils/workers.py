1936ebb68905d985d3eddf6ca627ce6da36e1f98
Goro <amstl86@gmail.com>
2026-03-22 11:48:29 +0700

    Stream for downloading emails.

--- mail_client/utils/workers.py
+++ mail_client/utils/workers.py
@@ -1,8 +1,10 @@
 # utils/workers.py
-
-from PyQt6.QtCore import QThread, pyqtSignal
 import imaplib
 import ssl
+
+from PyQt6.QtCore import QThread, pyqtSignal
+
+from mail_client.model.imap_client import IMAPClient
 
 class TestConnectionThread(QThread):
     """
@@ -26,5 +28,40 @@ class TestConnectionThread(QThread):
             imap.login(self.email, self.password)
             imap.logout()
             self.success.emit()
+        except Exception as e:
+            self.error.emit(str(e))
+            
+
+class FetchEmailsThread(QThread):
+    """
+    Поток для загрузки списка писем из папки.
+    """
+    finished = pyqtSignal(list)   # список EmailMessage
+    error = pyqtSignal(str)
+
+    def __init__(self, account, folder: str = "INBOX", limit: int = 50):
+        super().__init__()
+        self.account = account
+        self.folder = folder
+        self.limit = limit
+
+    def run(self):
+        try:
+            client = IMAPClient(
+                self.account.imap_server,
+                self.account.imap_port,
+                self.account.email,
+                self.account.get_password()
+            )
+            client.connect()
+            client.select_folder(self.folder)
+            uids = client.fetch_uids(self.limit)
+            emails = []
+            for uid in uids:
+                email_msg = client.fetch_email_by_uid(uid)
+                if email_msg:
+                    emails.append(email_msg)
+            client.logout()
+            self.finished.emit(emails)
         except Exception as e:
             self.error.emit(str(e))
